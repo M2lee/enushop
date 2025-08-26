748d25a94654c58482f41f335f60dbf74670d290
DESKTOP-QR3REVP\LEO <mrishku@gmail.com>
2025-08-26 21:06:42 +0900

    ASP test

---
+++ frontend/test_wep/MyWebApp/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+/*app.MapRazorPages()
+   .WithStaticAssets();*/
+app.MapRazorPages();
+app.MapGet("/", async context => {
+    context.Response.Redirect("/Index.html");
+});
+app.Run();
+
